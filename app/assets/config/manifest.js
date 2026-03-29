dd22fd6f5f0f018af8fa27f687a534f551bd4d80
Muhammad Shehram Salim <muhammadshehramsalim2005@gmail.com>
2026-03-29 10:44:23 +0100

    added the le wagon stylesheets

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,9 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+
+//= link_tree ../images
+//= link_tree ../../javascript .js
+//= link_directory ../stylesheets .css
+//= link popper.js
+//= link bootstrap.min.js
